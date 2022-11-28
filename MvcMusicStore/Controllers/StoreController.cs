9bbbb11cd7a362405d57cefc330f10dd1c709da0
Jarosław Świderski <jarekswiderski2@gmail.com>
2022-11-28 13:41:26 +0100

    Dodaj pliki projektów.

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcMusicStore.Models;
+
+namespace MvcMusicStore.Controllers
+{
+  
+    public class StoreController : Controller
+    {
+        MusicStoreEntities storeDB = new MusicStoreEntities();
+
+        public ActionResult Index()
+        {
+            var genres = storeDB.Genres.ToList();
+            return View(genres);
+        }
+    }
+
+
+}
---
+++ MvcMusicStore/Global.asax.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using System.Web.Security;
+using System.Web.SessionState;
+using System.Web.Http;
+using System.Data.Entity;
+
+namespace MvcMusicStore
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start()
+        {
+            System.Data.Entity.Database.SetInitializer(
+            new MvcMusicStore.Models.SampleData());
+            AreaRegistration.RegisterAllAreas();
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
